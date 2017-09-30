ae8c1064906ad75fae787a3732d83273774f9cc0
H Lalramenga <32388233+ramenga@users.noreply.github.com>
2017-09-30 16:47:01 +0530

    Create helloworld-python1.py

---
+++ helloworld-python1.py
@@ -0,0 +1,11 @@
+import time as darsana
+'''
+  Python hello world
+  darsana is dar + sana
+  dar means time in Mizo language
+  sana means a watch or a clock in Mizo language
+'''
+
+print("The time is:")
+print(str(darsana.time()))
+print("\n HELLO WORLD")
